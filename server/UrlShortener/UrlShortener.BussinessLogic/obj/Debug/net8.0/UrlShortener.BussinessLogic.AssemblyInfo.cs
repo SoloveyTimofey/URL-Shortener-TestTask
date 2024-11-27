920938ae2f8a86ee4bc706486b9cd195c8f963b5
Тимофей <116516370+SoloveyTimofey@users.noreply.github.com>
2024-11-27 11:15:29 +0200

    Initial2

--- server/UrlShortener/UrlShortener.BussinessLogic/obj/Debug/net8.0/UrlShortener.BussinessLogic.AssemblyInfo.cs
+++ server/UrlShortener/UrlShortener.BussinessLogic/obj/Debug/net8.0/UrlShortener.BussinessLogic.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UrlShortener.BussinessLogic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91b4d1ea1cad410d58bb98e5b452c1ea2a26aa11")]
 [assembly: System.Reflection.AssemblyProductAttribute("UrlShortener.BussinessLogic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UrlShortener.BussinessLogic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
